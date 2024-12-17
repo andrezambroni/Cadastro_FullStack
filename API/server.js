1a7f1bfac3ef1e34723bd025c3b526635b0028b4
andrezambroni <andrezambroni@gmail.com>
2024-12-17 14:11:05 -0300

    feat: adicionando Params

--- API/server.js
+++ API/server.js
@@ -1,10 +1,17 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
-app.get("/usuarios", (req, res) => {
-  res.send("ok, rota funcionando")
+const users = []
+app.post("/usuarios", (req, res) => {
+  users.push(req.body)
+
+  res.send("ok, post")
 })
 
+app.get("/usuarios", (req, res) => {
+  res.json(users)
+})
 
 app.listen(3000)
